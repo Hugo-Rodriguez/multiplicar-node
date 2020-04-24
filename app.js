a62bbe123cfcda082a3f2bcdbad12fc5682434c6
hugo <hugo_rodriguez@outlook.com>
2020-04-24 21:05:24 +0200

    commit inicial

---
+++ app.js
@@ -0,0 +1,30 @@
+console.clear();
+
+const argv = require("./config/yargs").argv;
+const colors = require("colors");
+
+const { crearArchivo, listarTabla } = require("./multiplicar/multiplicar");
+
+let comando = argv._[0];
+
+switch (comando) {
+    case "listar":
+        console.log("listar");
+        listarTabla(argv.base, argv.limite);
+        break;
+    case "crear":
+        console.log("crear");
+        crearArchivo(argv.base, argv.limite)
+            .then((archivo) =>
+                console.log(
+                    // `Archivo creado: "tabla-${argv.base}-al-${argv.limite}.txt"`
+                    `Archivo creado: ${archivo.green}`
+                )
+            )
+            .catch((e) => console.log(e));
+        break;
+    default:
+        console.log("Comando no reconocido");
+}
+
+let argv2 = process.argv;
